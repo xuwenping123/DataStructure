d99cc1b2591171aceb681a5e04adc05fc0c189f5
徐文平 <xuwp@nfex.com>
2017-11-02 09:21:56 +0800

    [mod] mod Q_1_1_21

--- src/Test.java
+++ src/Test.java
@@ -6,6 +6,15 @@ public class Test {
 	}
 	
 	public static void main(String[] args) {
-		Test.test1();
+		double d1 = 218951036.00d;
+		double d2 = 1555143d;
+		double d3 = 3635303d;
+		double d4 = 3490767d;
+		double d5 = 4130369d;
+		double d6 = 206139454d;
+		System.out.println(d1 - d2 - d3 - d4 - d5 - d6);
+		
+		System.out.println(2000.00 + 178978.00 + 3534980.00 + 4362259.00 + 40348956.00 + 49860923.00
+				+ 5480911.00 + 772949.00 + 76085484.00 + 15457166.00 + 2009080.00 + 1271100.00 + 18856149.00 + 717665.00 +12286.00 +  150.00 );
 	}
 }
--- src/chapter01_fundamentals/exercises/Q_1_1_21.java
+++ src/chapter01_fundamentals/exercises/Q_1_1_21.java
@@ -1,5 +1,6 @@
 package chapter01_fundamentals.exercises;
 
+import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 import edu.princeton.cs.algs4.StdIn;
@@ -27,11 +28,39 @@ public class Q_1_1_21 {
 		Record record;
 		for (int i = 0; i < list.size(); i++) {
 			record = list.get(i);
-			StdOut.print(record.getName() + "\t" + record.num1 + "\t" + record.num2 + "\t" + record.rate);
+			StdOut.print((i + 1) + "\t" + record.getName() + "\t" + record.num1 + "\t" + record.num2 + "\t" + round(record.rate, 3));
 		}
+		StdOut.println();
+	}
+	
+	/**
+	  * @Title: round
+	  * @Description: 数值精度控制
+	  * @author: xuwenping
+	  * @date: 2017年10月25日
+	  * @param v	需要控制精度的数值
+	  * @param scale  小数位数
+	  * @return	
+	 */
+	public static float round(float v, int scale) { 
+		if (scale < 0) { 	
+			return v;
+		} 
+		if(v == 0d || (v + "").equals("NaN")){
+			return 0;
+		}
+		BigDecimal b = new BigDecimal(Float.toString(v)); 
+		return b.setScale(scale, BigDecimal.ROUND_HALF_UP).floatValue(); 
+	} 
+	
+	public static void main(String[] args) {
+		test();
 	}
 }
 
+/**
+ * 使用对象，存储数据 
+ */
 class Record {
 	String name;
 	int num1;
@@ -42,7 +71,7 @@ class Record {
 		this.name = name;
 		this.num1 = num1;
 		this.num2 = num2;
-		this.rate = Math.scalb((float)num1 / num2, 3);
+		this.rate = 1.0f * num1 / num2;
 	}
 
 	public String getName() {
