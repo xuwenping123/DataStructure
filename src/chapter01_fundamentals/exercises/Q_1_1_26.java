916b32445f8dd84178507bcdbd22572e74b25e4f
徐文平 <xuwp@nfex.com>
2017-11-03 18:03:51 +0800

    add 26

---
+++ src/chapter01_fundamentals/exercises/Q_1_1_26.java
@@ -0,0 +1,35 @@
+package chapter01_fundamentals.exercises;
+
+public class Q_1_1_26 {
+
+	public static void main(String[] args) {
+		sort(235, 2, 24);
+	}
+
+	/*
+	 * 对于类型的泛型失败
+	 */
+	public static <T extends Number> void sort(T a, T b, T c) {
+		T t;
+	}
+	
+	public static void sort(int a, int b, int c) {
+		int t;
+		if (a > b) {
+			t = a;
+			a = b;
+			b = t;
+		} 
+		if (a > c) {
+			t = a;
+			a = c;
+			c = t;
+		}
+		if (b > c) {
+			t = b;
+			b = c;
+			c = t;
+		}
+		System.out.println(a + "  " + b + "  " + c);
+	}
+}
