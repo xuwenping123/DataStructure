cd8c187e06bccb373adb8c395959fbb5db810dbb
xuwenping123 <wenpingxu123@163.com>
2017-11-06 23:28:04 +0800

    [add] part02/StringOperation.java

---
+++ src/chapter01_fundamentals/part02/StringOperation.java
@@ -0,0 +1,36 @@
+package chapter01_fundamentals.part02;
+
+/**
+ * @ClassName: StringOperation 
+ * @Description: 字符串的典型操作，见书p50
+ * @author xuwenping
+ * @date 2017年11月6日 下午8:27:13 
+ * @version V1.0
+ */
+public class StringOperation {
+
+	/**
+	 * @Title: checkString 
+	 * @Description: 校验字符串 
+	 * @param object
+	 */
+	private static void checkString(Object object) {
+		
+	}
+	
+	/**
+	 * @Title: isPalindrome 
+	 * @Description: 判断字符串是否为回字文
+	 * @param s
+	 * @return boolean
+	 * 	if s is palindrome ,return true
+	 * 	else return false
+	 */
+	public static boolean isPalindrome(String s) {
+		checkString(s);
+		boolean flag = true;
+		int start = 0;
+		int end = s.length() - 1;
+		
+	}
+}
