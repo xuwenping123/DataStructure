271a1896cade58bb5380eb60f37949f69a8e9ce4
xuwenping <wenpingxu123@163.com>
2017-11-20 21:20:59 +0800

    node

---
+++ src/chapter01_fundamentals/part03/node/Node.java
@@ -0,0 +1,25 @@
+package chapter01_fundamentals.part03.node;
+
+/**
+ * @ClassName: Node 
+ * @Description: see P90, 链表的结点类
+ * 
+ * 链表的基本操作有：
+ * 
+ * 1. 在表头插入结点
+ * 2. 在表头删除结点
+ * 3. 在表尾插入结点
+ * 4. 删除指定的结点
+ * 5. 在指定的结点前插入一个新结点
+ * 6. 数组中元素的遍历
+ * 
+ * @author xuwenping
+ * @date 2017年11月18日 下午9:43:16 
+ * @version V1.0
+ * @param <Item>
+ */
+public class Node <Item>{
+
+	Item item;//存储数据
+	Node node;// 
+}
